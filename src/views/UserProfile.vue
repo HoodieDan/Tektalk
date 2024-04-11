7c2a51a7c94c24a63823fea86571717f89344fa8
hoodiedan <obodedaniel3@gmail.com>
2024-04-11 10:26:13 +0100

    hid chat button for unauthorised and same users

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -117,7 +117,7 @@
             </div>
 
             <div class="others mt-3">
-                <router-link :to="{ name: 'Chat', params: { id: profile.userId } }" class="circle" v-if="(profile !== loggedInUser) && (loggedInUser !== null)">
+                <router-link :to="{ name: 'Chat', params: { id: profile.userId } }" class="circle" v-if="(profile.username !== loggedInUser.username) && (loggedInUser !== null)">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16">
                         <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
                     </svg>
@@ -271,7 +271,6 @@ export default {
 
         let posts;
         let talks;
-        let profile = null;
 
         if (user_profile) {
             if (user_profile.status === 200) {
@@ -316,6 +315,8 @@ export default {
 
             this.loggedInUser = auth.user;
             this.profile = user_profile.data;
+            console.log(this.profile);
+            console.log(this.loggedInUser);
         }
     },
     mounted() {
