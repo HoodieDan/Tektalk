859857000be94cdb5fd799595b47d1ea93f92804
hoodiedan <obodedaniel3@gmail.com>
2024-04-11 09:09:38 +0100

    changed mobile bottom menu icons to svg's

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="bottom-menu" v-if="currentRoute !== 'Chat'">
     <div class="route">
-        <router-link :to="{ name: 'Home' }"><i class="fa fa-solid fa-house" :class="{ 'active': currentRoute === 'Home' }" ></i></router-link>
+        <router-link :to="{ name: 'Home' }" :class="{ 'active': currentRoute === 'Home' }">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FFF" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
+                <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+            </svg>
+        </router-link>
     </div>
     <div class="route">
-        <router-link :to="{ name: 'Talks' }"><i class="fa fa-solid fa-chart-line" :class="{ 'active': currentRoute === 'Talks' }" ></i></router-link>
+        <router-link :to="{ name: 'Talks' }" :class="{ 'active': currentRoute === 'Talks' }">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FFF" class="bi bi-laptop" viewBox="0 0 16 16">
+                <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5"/>
+            </svg>
+        </router-link>
     </div>
     <div class="route">
-        <router-link :to="{ name: 'Network' }"><i class="fa fa-solid fa-users" :class="{ 'active': currentRoute === 'Network' }" ></i></router-link>
+        <router-link :to="{ name: 'Network' }" :class="{ 'active': currentRoute === 'Network' }">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="#FFF" class="bi bi-people" viewBox="0 0 16 16">
+                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+            </svg>
+        </router-link>
     </div>
     <div class="route">
-        <router-link :to="{ name: 'Events' }"><i class="fa fa-regular fa-address-book" :class="{ 'active': currentRoute === 'Events' }" ></i></router-link>
+        <router-link :to="{ name: 'Events' }" :class="{ 'active': currentRoute === 'Events' }">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FFF" class="bi bi-calendar2-event" viewBox="0 0 16 16">
+                <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/>
+                <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        </router-link>
     </div>
     <div class="route">
-        <router-link :to="{ name: 'Settings' }"><i class="fa fa-solid fa-gear" :class="{ 'active': currentRoute === 'Settings' }" ></i></router-link>
+        <router-link :to="{ name: 'Settings' }" :class="{ 'active': currentRoute === 'Settings' }">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FFF" class="bi bi-gear" viewBox="0 0 16 16">
+                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
+                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
+            </svg>
+        </router-link>
     </div>
   </div>
 </template>
@@ -33,8 +57,8 @@ export default {
 .bottom-menu {
    position: fixed;
    width: 100%;
-   padding-top: 0.5rem;
-   padding-bottom: 0.5rem;
+   padding-top: 0.8rem;
+   padding-bottom: 0.8rem;
    background-color: #000;
    z-index: 1000;
    bottom: 0;
@@ -45,6 +69,9 @@ export default {
 }
 .fa {
     color: #e7e9ea;
+}
+.active > * {
+    fill: #01BAEF;
 }
 @media (min-width: 769px) {
     .bottom-menu {
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -51,7 +51,7 @@
             <h6 class="mb-0">{{ currentRoute }}</h6>
         </div>
         <div class="col-3 d-flex mobile-icons right-border" v-if="currentUser !== null">
-            <router-link :to="{ name: 'Chats' }" class="light no-underline pe-4" >
+            <router-link :to="{ name: 'Chats' }" class="light no-underline pe-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send fai" viewBox="0 0 16 16" :class="{ 'active': currentRoute === 'Chats' }">
                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
                 </svg>
